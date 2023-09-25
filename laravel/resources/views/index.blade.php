39eec10f6d360a90f6ef1adf8e62c62551a5d9d9
Sije Harkema <harkemasije@gmail.com>
2023-09-25 20:58:55 +1000

    Quick first draft of content

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,5 +1,96 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    {{-- I don't like styling. Thank you https://startbootstrap.com/theme/resume --}}
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <title>Resume - Start Bootstrap Theme</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico"/>
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.4.2/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
+          type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="assets/css/app.css" rel="stylesheet"/>
+</head>
+<body id="page-top">
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Sije Harkema</span>
+        <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2"
+                                             src="assets/img/profile.png" alt="..."/></span>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span
+            class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#technology">Technology</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#religion">Religion</a></li>
+            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#misc">Misc</a></li>
+        </ul>
+    </div>
+</nav>
+<!-- Page Content-->
+<div class="container-fluid p-0">
+    <section class="resume-section" id="about">
+        <div class="resume-section-content">
+            <h1 class="mb-0">
+                Sije
+                <span class="text-primary">Harkema</span>
+            </h1>
+            <div class="subheading mb-5">
+                <i class="fa-solid fa-location-dot"></i> Between Urk [NL] and Kapuna [PNG] ·
+                <a href="mailto:harkemasije[at]gmail.com">harkemasije[at]gmail.com</a>
+            </div>
+            <p class="lead mb-5">Welcome dear visitor! I love technology and especially learning new paradigms and how
+                it can fit as puzzle pieces together. I love my family, and above all I love God. This website is
+                specifically not a resume. On different subjects I might write some blog-like posts to help me develop
+                my thoughts. I will use this domain to host some tinkering things or useful mini-projects.</p>
 
-@section('body')
-    localhost
-@endsection
+            <p class="lead mb-5">About this website's tech stack: I love piecing stuff together but do not like to do
+                website styling. This is a simple template I got from the internet which suffices. This website
+                runs in a few
+                Docker containers pieced together with Docker Compose. The source is up on <a
+                    href="https://github.com/afraca/sije.harkema.xyz">GitHub</a>.</p>
+            <div class="social-icons">
+                <a class="social-icon" href="https://www.linkedin.com/in/sije-harkema/"><i
+                        class="fab fa-linkedin-in"></i></a>
+                <a class="social-icon" href="https://github.com/afraca"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+    </section>
+    <hr class="m-0"/>
+    <section class="resume-section" id="technology">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Technology</h2>
+            <p>Things that I love: </p>
+            <ul>
+            </ul>
+        </div>
+    </section>
+    <hr class="m-0"/>
+    <section class="resume-section" id="religion">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Religion</h2>
+        </div>
+    </section>
+    <section class="resume-section" id="misc">
+        <div class="resume-section-content">
+            <h2 class="mb-5">Misc</h2>
+        </div>
+    </section>
+    <hr class="m-0"/>
+</div>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="assets/js/app.js"></script>
+</body>
+</html>
