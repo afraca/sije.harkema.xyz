67e2f406419490f82cb7d005f65a9c7615ddfe8b
Sije Harkema <harkemasije@gmail.com>
2023-09-25 16:53:06 +1000

    Empty homepage init

--- laravel/app/Http/Controllers/Controller.php
+++ laravel/app/Http/Controllers/Controller.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function home(): Response
+    {
+        return new Response(view('index'));
+    }
 }
